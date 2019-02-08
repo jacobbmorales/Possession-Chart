9e5d1ad68d46d12074b9982c259c2234554aca87
Jacob Morales <jacob.morales@colorado.edu>
2019-02-08 10:56:23 -0700

    removes localhost

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config(object):
     MYSQL_DATABASE_USER = 'jacob.morales@focus.org'
     MYSQL_DATABASE_PASSWORD = 'Rufus1209'
     MYSQL_DATABASE_DB = 'mydb'
-    MYSQL_DATABASE_HOST = 'localhost'
+    CLEARDB_DATABASE_URL = 'mysql://b91af171564b1c:a818600b@us-cdbr-iron-east-03.cleardb.net/heroku_e927b2471f3d26b?reconnect=true'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
