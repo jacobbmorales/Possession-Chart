c67c3430d5df034262b0d99d508fc5642802c459
Jacob Morales <jacob.morales@colorado.edu>
2019-02-08 13:06:18 -0700

    changed bind address

--- app/config.py
+++ app/config.py
@@ -6,8 +6,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_strong_key')
+    MYSQL_DATABASE_USER = 'jacob.morales@focus.org'
+    MYSQL_DATABASE_PASSWORD = 'Rufus1209'
+    MYSQL_DATABASE_DB = 'mydb'
     CLEARDB_DATABASE_URL = 'mysql://b91af171564b1c:a818600b@us-cdbr-iron-east-03.cleardb.net/heroku_e927b2471f3d26b?reconnect=true'
-    CLEARDB_MAROON_URL =   'mysql://b900e042a0b297:127eabae@us-cdbr-iron-east-03.cleardb.net/heroku_c5f672dc0e40eb4?reconnect=true'
-    DATABASE_URL =         'mysql://b91af171564b1c:a818600b@us-cdbr-iron-east-03.cleardb.net/heroku_e927b2471f3d26b?reconnect=true'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    CLEARDB_MAROON_URL = 'mysql://b900e042a0b297:127eabae@us-cdbr-iron-east-03.cleardb.net/heroku_c5f672dc0e40eb4?reconnect=true'
+    DATABASE_URL = 'mysql://b91af171564b1c:a818600b@us-cdbr-iron-east-03.cleardb.net/heroku_e927b2471f3d26b?reconnect=true'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
